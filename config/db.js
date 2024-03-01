843b444593c0fcadbf679981f6e895f049d93185
BillEddyM <billeddydaniel2018@gmail.com>
2024-02-29 21:21:19 -0600

    iniciando, cambiar a lenovo

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import colors from 'colors'
 
 export const db = async() => {
 
     try {
-        const db = await mongoose.connect('mongodb+srv://root:root@cluster0.vo8b0ts.mongodb.net/?retryWrites=true&w=majority')
-        
-        console.log('Se conecto a la db correctamente')
+        const db = await mongoose.connect(process.env.MONGO_URL)
+        console.log(db.connection)
+        const url  = `$(db.connection.host):$(db.connection.port)`
+        console.log(colors.america(url))
     } catch (error) {
         console.log(error);
         process.exit(1);
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 
 import express from 'express' //esm modules javascript
+import dotenv from 'dotenv' //
 import { db } from './config/db.js'
 import servicesRoutes from './routes/servicesRoutes.js'
+import colors from 'colors'
+
+dotenv.config() 
 
 // config la app 
 const app = express()
 
 //conectar a la DB
 db()
+
 
 //def ruta 
 app.use('/api/services', servicesRoutes)
@@ -18,4 +23,8 @@ const PORT = process.env.PORT || 4000
 
 //arrancar app 
 app.listen(PORT, () => {
-    console.log('El servidor se esta ejecutadon en el puerto:', PORT)})
+    console.log( colors.blue('El servidor se esta ejecutadon en el puerto:', PORT))
+    
+})
+
+
